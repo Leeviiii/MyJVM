05166de0025cd29230d32a919549dbbbccda5f88
yangfei5 <yangfei5@xiaomi.com>
2018-07-30 17:00:16 +0800

    run time data area

--- src/jvmgo/main.go
+++ src/jvmgo/main.go
@@ -4,6 +4,7 @@ import "fmt"
 import "strings"
 import "jvmgo/classpath"
 import "jvmgo/classfile"
+import "jvmgo/rtda"
 
 // ./jvmgo java.lang.Object
 func main() {
@@ -22,6 +23,41 @@ func startJVM(cmd *Cmd) {
 	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
 	cf := loadClass(className, cp)
 	printClsasInfo(cf)
+	frame := rtda.NewFrame(100, 100)
+	testLocalVars(frame.LocalVars())
+	testOperandStack(frame.OperandStack())
+}
+func testLocalVars(vars rtda.LocalVars) {
+	vars.SetInt(0, 100)
+	vars.SetInt(1, -100)
+	vars.SetLong(2, 2997924580)
+	vars.SetLong(4, -2997924580)
+	vars.SetFloat(6, 3.1415)
+	vars.SetDouble(7, 2.71828182845)
+	vars.SetRef(9, nil)
+	fmt.Println(vars.GetInt(0))
+	fmt.Println(vars.GetInt(1))
+	fmt.Println(vars.GetLong(2))
+	fmt.Println(vars.GetLong(4))
+	fmt.Println(vars.GetFloat(6))
+	fmt.Println(vars.GetDouble(7))
+	fmt.Println(vars.GetRef(9))
+}
+func testOperandStack(ops *rtda.OperandStack) {
+	ops.PushInt(100)
+	ops.PushInt(-100)
+	ops.PushLong(2997924580)
+	ops.PushLong(-2997924580)
+	ops.PushFloat(3.1415)
+	ops.PushDouble(2.71828182845)
+	ops.PushRef(nil)
+	fmt.Println(ops.PopRef())
+	fmt.Println(ops.PopDouble())
+	fmt.Println(ops.PopFloat())
+	fmt.Println(ops.PopLong())
+	fmt.Println(ops.PopLong())
+	fmt.Println(ops.PopInt())
+	fmt.Println(ops.PopInt())
 }
 func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
 	classData, _, err := cp.ReadClass(className)
